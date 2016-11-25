5dc175187074cfa969331410d218bba7ab236709
derushio <narumi18wa@gmail.com>
2016-11-26 00:31:34 +0900

    remove jquery index.html

--- pullsrc.sh
+++ pullsrc.sh
@@ -9,5 +9,3 @@ npm install
 
 cd ../
 bower install
-sed -i -e 's|<link rel="stylesheet" href="dist/markdown-plus.min.css"/>|<link rel="stylesheet" href="dist/markdown-plus.min.css"/>\
-    <script src="../bower_components/jquery/dist/jquery.min.js"></script>|g' ./src/index.html
