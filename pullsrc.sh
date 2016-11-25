a89f40e44a1cc8a2ad1f3489e943f1a45db4b2b4
derushio <narumi18wa@gmail.com>
2016-11-25 21:13:26 +0900

    fix pullsrc

--- pullsrc.sh
+++ pullsrc.sh
@@ -2,6 +2,12 @@
 
 rm -rf ./src
 cp -R ../../web/markdown-plus ./src
+
 cd src
 rm -rf ./node_modules
 npm install
+
+cd ../
+bower install
+sed -i -e 's|<link rel="stylesheet" href="dist/markdown-plus.min.css"/>|<link rel="stylesheet" href="dist/markdown-plus.min.css"/>\
+    <script src="../bower_components/jquery/dist/jquery.min.js"></script>|g' ./src/index.html
