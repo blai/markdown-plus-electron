bf748a2e2e3570d0f2c0784ee1d52c8cd6828a13
derushio <narumi18wa@gmail.com>
2016-11-26 19:48:11 +0900

    fix 動かない表記法を修正

--- window/window.js
+++ window/window.js
@@ -1,6 +1,6 @@
 $(function() {
-    $('#saveButton').click(function(e) {
-        var editor = $(".editor")[0].contentWindow.editor;
+    $('#btn-save').click(function(e) {
+        var editor = $(".editor-markdown")[0].contentWindow.editor;
         console.log(editor.session.getValue());
     });
 });
