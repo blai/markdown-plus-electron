b9de418cb5f0b88a21c91986343f0bc0ea62d24c
derushio <narumi18wa@gmail.com>
2016-11-28 23:54:52 +0900

    add 線グラフ

--- window/window.js
+++ window/window.js
@@ -16,6 +16,10 @@ class EditorWindow {
                 this.editor = $('.editor-markdown')[0].contentWindow.editor;
             });
 
+            $('#editor-controll-chart-line').click(event, () => {
+                this.editor.insert('```chart\n{\n  "type": "line",\n  "data": {\n    "labels": [\n      "January",\n      "February",\n      "March",\n      "April",\n      "May",\n      "June",\n      "July"\n    ],\n    "datasets": [\n      {\n        "label": "# of bugs",\n        "fill": false,\n        "lineTension": 0.1,\n        "backgroundColor": "rgba(75,192,192,0.4)",\n        "borderColor": "rgba(75,192,192,1)",\n        "borderCapStyle": "butt",\n        "borderDash": [],\n        "borderDashOffset": 0,\n        "borderJoinStyle": "miter",\n        "pointBorderColor": "rgba(75,192,192,1)",\n        "pointBackgroundColor": "#fff",\n        "pointBorderWidth": 1,\n        "pointHoverRadius": 5,\n        "pointHoverBackgroundColor": "rgba(75,192,192,1)",\n        "pointHoverBorderColor": "rgba(220,220,220,1)",\n        "pointHoverBorderWidth": 2,\n        "pointRadius": 1,\n        "pointHitRadius": 10,\n        "data": [\n          65,\n          59,\n          80,\n          81,\n          56,\n          55,\n          40\n        ],\n        "spanGaps": false\n      }\n    ]\n  },\n  "options": {}\n}\n```');
+            });
+
             this.resizeEditor();
             this.registerGlobalMessage();
         });
